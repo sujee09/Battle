6ebe0a1f0bc4ccff2fdd1f6623d77cd831b7d0d8
Chris Abdo <christophermabdo@icloud.com>
2020-12-08 17:34:06 +0000

    creating test infrastructure

--- Gemfile
+++ Gemfile
@@ -7,5 +7,5 @@ git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 # gem "rails"
 
 gem 'rspec'
-
+gem 'capybara'
 gem 'sinatra'
---
+++ spec/features/testing_infrastructure_spec.rb
@@ -0,0 +1,6 @@
+feature 'Testing infrastructure' do
+    scenario 'Can run app and change page content' do
+        visit('/')
+        expect(page).to have_content 'Testing infrastructure working!'
+    end
+end
