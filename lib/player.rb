bd6404ab37a4a186a63959ca6e851aee55582c3d
Sujeevan Rajagopalan <rsujee@MA307.local>
2020-12-10 16:41:55 +0000

    Implementing Hitpoints in porgress

--- lib/player.rb
+++ lib/player.rb
@@ -8,4 +8,12 @@ class Player
     @hitpoints = hitpoints
   end
 
+  def attack(player)
+    player.receive_damage
+  end
+
+  def receive_damage
+    @hitpoints -= 10
+  end
+
 end
--- spec/units/player_spec.rb
+++ spec/units/player_spec.rb
@@ -1,10 +1,36 @@
 require 'player'
 
 describe Player do
-  subject(:player) { Player.new('Suje') }
+  subject(:pikachu) { Player.new('Pikachu') }
+  subject(:meowth) {Player.new('Meowth') }
 
-  it 'creates a new instance of a player with the name Suje' do
-    expect(subject.name).to eq('Suje')
+  describe '#names' do
+    it 'creates a new instance of a player with the name Pikachu' do
+      expect(pikachu.name).to eq('Pikachu')
+    end
+
+    it 'creates a new instance of a player with the name Meowth' do
+      expect(meowth.name).to eq('Meowth')
+    end
   end
 
+  describe '#hitpoints' do
+    it 'returns the hitpoints' do
+      expect(pikachu.hitpoints).to eq described_class::DEFAULT_HITPOINTS
+    end
+  end
+
+  describe '#attack' do
+    it 'damages the player' do
+      expect(meowth).to  receive(:receive_damage)
+      pikachu.attack(meowth)
+    end
+  end
+
+  describe '#receive_damage' do
+    it 'reduces the players hitpoints' do
+      expect { meowth.receive_damage }.to change { meowth.hitpoints }.by(-10)
+    end
+  end
+  
 end
